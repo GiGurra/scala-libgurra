a456107bc0f5dd0258c9542e825bd4f948399ef7
Johan Kjölhede <gigurra@gmail.com>
2016-10-01 12:38:00 +0200

    Make zero static instances

--- src/main/scala/se/gigurra/math/Zero.scala
+++ src/main/scala/se/gigurra/math/Zero.scala
@@ -14,23 +14,23 @@ object Zero {
 
   def apply[@specialized(Primitives) T: Zero]: T = implicitly[Zero[T]].zero
 
-  implicit val intZero = new Zero[Int] { override def zero = 0 }
-  implicit val floatZero = new Zero[Float] { override def zero = 0.0f }
-  implicit val longZero = new Zero[Long] { override def zero = 0L }
-  implicit val doubleZero = new Zero[Double] { override def zero = 0.0 }
+  implicit val intZero = new Zero[Int] { override val zero = 0 }
+  implicit val floatZero = new Zero[Float] { override val zero = 0.0f }
+  implicit val longZero = new Zero[Long] { override val zero = 0L }
+  implicit val doubleZero = new Zero[Double] { override val zero = 0.0 }
 
-  implicit val intVec2Zero = new Zero[Vec2[Int]] { override def zero = Vec2() }
-  implicit val floatVec2Zero = new Zero[Vec2[Float]] { override def zero = Vec2() }
-  implicit val longVec2Zero = new Zero[Vec2[Long]] { override def zero = Vec2() }
-  implicit val doubleVec2Zero = new Zero[Vec2[Double]] { override def zero = Vec2() }
+  implicit val intVec2Zero = new Zero[Vec2[Int]] { override val zero = Vec2[Int]() }
+  implicit val floatVec2Zero = new Zero[Vec2[Float]] { override val zero = Vec2[Float]() }
+  implicit val longVec2Zero = new Zero[Vec2[Long]] { override val zero = Vec2[Long]() }
+  implicit val doubleVec2Zero = new Zero[Vec2[Double]] { override val zero = Vec2[Double]() }
 
-  implicit val intVec3Zero = new Zero[Vec3[Int]] { override def zero = Vec3() }
-  implicit val floatVec3Zero = new Zero[Vec3[Float]] { override def zero = Vec3() }
-  implicit val longVec3Zero = new Zero[Vec3[Long]] { override def zero = Vec3() }
-  implicit val doubleVec3Zero = new Zero[Vec3[Double]] { override def zero = Vec3() }
+  implicit val intVec3Zero = new Zero[Vec3[Int]] { override val zero = Vec3[Int]() }
+  implicit val floatVec3Zero = new Zero[Vec3[Float]] { override val zero = Vec3[Float]() }
+  implicit val longVec3Zero = new Zero[Vec3[Long]] { override val zero = Vec3[Long]() }
+  implicit val doubleVec3Zero = new Zero[Vec3[Double]] { override val zero = Vec3[Double]() }
 
-  implicit val intVec4Zero = new Zero[Vec4[Int]] { override def zero = Vec4() }
-  implicit val floatVec4Zero = new Zero[Vec4[Float]] { override def zero = Vec4() }
-  implicit val longVec4Zero = new Zero[Vec4[Long]] { override def zero = Vec4() }
-  implicit val doubleVec4Zero = new Zero[Vec4[Double]] { override def zero = Vec4() }
+  implicit val intVec4Zero = new Zero[Vec4[Int]] { override val zero = Vec4[Int]() }
+  implicit val floatVec4Zero = new Zero[Vec4[Float]] { override val zero = Vec4[Float]() }
+  implicit val longVec4Zero = new Zero[Vec4[Long]] { override val zero = Vec4[Long]() }
+  implicit val doubleVec4Zero = new Zero[Vec4[Double]] { override val zero = Vec4[Double]() }
 }
